e4a48c4c851aa67781a8c6a428ec27353077947b
Aditya Naravane <adityanaravane2008@gmail.com>
2022-10-30 18:38:47 -0400

    adding splash screen

--- SolarBoost/splash screen.swift
+++ SolarBoost/splash screen.swift
@@ -7,16 +7,38 @@
 
 import SwiftUI
 
+
+
+extension UserDefaults {
+    
+    var welcomeScreenShown: Bool {
+        get {
+            return (UserDefaults.standard.value(forKey: "welcomeScreenShown") as? Bool) ?? false
+        }
+        
+        set {
+            UserDefaults.standard.setValue(newValue, forKey: "welcomeScreenShown")
+        }
+    }
+    
+}
+
 struct splash_screen: View {
     
     @State private var isActive = false
     @State private var size = 0.8
     @State private var opacity = 0.5
     
+    
     var body: some View {
         
         if isActive {
-            splashview2()
+            if UserDefaults.standard.welcomeScreenShown {
+                MainView()
+            } else {
+                splashview2()
+            }
+
         } else {
             ZStack {
                 Color.gray
@@ -54,6 +76,71 @@ struct splash_screen: View {
         
         
     }
+}
+
+
+struct splashview2: View {
+    var body: some View {
+        
+
+        
+        
+        NavigationView(content: {
+                VStack  {
+                    Text("Solar Panel Tilt Calculator")
+                        .font(.title)
+                        .padding()
+                    
+                    Image("25")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        
+                    ScrollView {
+                        Text("To calculate the tilt, we need to know the latitude of a place. Latitude is a coordinate that specifies the north–south position of a point on the surface of the Earth. 69 miles is one degree of latitude.")
+                            .font(.title3)
+                            .padding()
+                        
+                        
+                        Text("In this app, the user can select their town, and that pulls up the latitude of the place and it calculates the tilt of the solar panels for that month. It also shows the energy output if you choose to use only one tilt setting for all of winter, summer, spring and fall.")
+                            .font(.title3)
+                            .padding()
+                        
+                        
+                        Text("Changing the tilt of the solar panels can increase your energy production by upto 40%.")
+                            .font(.title3)
+                            .padding()
+                        
+                        Text("The formula for angle in the northern hemisphere is: 1.3793 + latitude * (1.2011 + latitude * (-0.014404 + latitude * 0.000080509)). The formula for the southern hemisphere is: -0.41657 + latitude * (1.4216 + latitude * (0.024051 + latitude * 0.00021828)))")
+                            .font(.title3)
+                            .padding()
+                        
+                        
+                        
+                        NavigationLink(destination: {
+                            ContentView()
+                            
+                        }, label: {
+                            Text("Get Started")
+                            
+                            
+                        })
+                        .foregroundColor(Color.white)
+                        .padding(10)
+                        .background(Color.blue)
+                        .cornerRadius(35)
+                        
+                    }
+                    .padding(.top)
+                    
+                }
+                .padding(.top)
+                .onAppear(perform: {
+                    UserDefaults.standard.welcomeScreenShown = true
+                })
+        })
+        
+    }
+        
 }
 
 struct splash_screen_Previews: PreviewProvider {
